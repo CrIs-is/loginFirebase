921046876c6099035d82af49afce3dd397324340
CrI-is <cdatehortua62@misena.edu.co>
2020-02-16 23:55:13 -0500

    añadiendo rutas y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomePageComponent},
+  {path:"login",component:LoginPageComponent},
+  {path:"singup",component:RegisterPageComponent},
+  {path:"home",component:HomePageComponent},
+  {path:"**",component:NotFoundPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
