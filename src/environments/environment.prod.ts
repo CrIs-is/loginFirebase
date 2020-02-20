13b4eae6a26ed82f18bcc5220b73c3aad913e4de
CrI-is <cdatehortua62@misena.edu.co>
2020-02-19 23:38:41 -0500

    agregando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDiapPcmpOQryeLDOt9s6PcidDneHfgjUE",
+    authDomain: "loginfirebase-11718.firebaseapp.com",
+    databaseURL: "https://loginfirebase-11718.firebaseio.com",
+    projectId: "loginfirebase-11718",
+    storageBucket: "loginfirebase-11718.appspot.com",
+    messagingSenderId: "229243962774",
+    appId: "1:229243962774:web:69777a44d75802d11289b7",
+    measurementId: "G-BV2XT1SX9H"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDiapPcmpOQryeLDOt9s6PcidDneHfgjUE",
+    authDomain: "loginfirebase-11718.firebaseapp.com",
+    databaseURL: "https://loginfirebase-11718.firebaseio.com",
+    projectId: "loginfirebase-11718",
+    storageBucket: "loginfirebase-11718.appspot.com",
+    messagingSenderId: "229243962774",
+    appId: "1:229243962774:web:69777a44d75802d11289b7",
+    measurementId: "G-BV2XT1SX9H"
+  }
 };
 
 /*
